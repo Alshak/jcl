299b9c8adb7af07126fc2393c34688df603d267a
Alain Shakour <ashakour@unistra.fr>
2016-03-01 16:40:27 +0100

    Samarah by HybridClassification

--- jcl/methods/samarah.py
+++ jcl/methods/samarah.py
@@ -1,36 +1,45 @@
 import os
 import jpype as jp
 import tempfile
+import time
 
 class Samarah(object):
     def __init__(self, agents):
         self.array_list = []
-        self.agents = agents
-        return None
-                
-    def add(self, data, params):       
-        if not jp.isThreadAttachedToJVM():
-            jp.attachThreadToJVM()
-        SamarahAgent = jp.JClass('jcl.learning.methods.multistrategy.evosamarah.myversion.SamarahAgent')
-        agent = SamarahAgent(data,params.getLearningMethodInstance())
-        self.array_list.append(agent)
-        
+        self.agents_python = agents
+        return None      
 
     def run(self):
         if not jp.isThreadAttachedToJVM():
             jp.attachThreadToJVM()
             
-        for agent in self.agents:
-            self.add(agent.get_data(), agent.get_parameters())
+        HybridClassification = jp.JClass('jcl.learning.methods.multistrategy.samarah.HybridClassification')
+        self.hybrid_classification = HybridClassification()
         
-        ExClassification = jp.JClass('jcl.learning.methods.multistrategy.evosamarah.myversion.ExClassification')
-        SetOfSamarahAgents = jp.JClass('jcl.learning.methods.multistrategy.evosamarah.myversion.SetOfSamarahAgents')
-        ArrayList = jp.JClass('java.util.ArrayList')
-        my_list = ArrayList()
-        for agent in self.array_list:
-            my_list.add(agent)
-        samarah_set = SetOfSamarahAgents(my_list)
-        exc = ExClassification(samarah_set)
-        exc.classify()
-        return ''
+        for agent_python in self.agents_python:
+            self.hybrid_classification.addAgent(agent_python.get_parameters(), agent_python.get_data())
+
+        self.hybrid_classification.classify()
+        clusRes = self.hybrid_classification.getClusteringResult()
         
+        temp_file_url = tempfile.mkdtemp()
+        timenow = int(round(time.time() * 1000))
+        output_file_url=os.path.join(temp_file_url,"result%s.arff"%str(timenow))
+        
+        ARFFWriter = jp.JClass('jcl.io.arff.ARFFWriter')   
+        writer = ARFFWriter(output_file_url,clusRes)
+        writer.write()
+
+        with open(output_file_url, 'r') as f:
+            output_file = f.read()
+        
+        return output_file
+    
+    def get_parameters(self):
+        raise NotImplementedError()
+    
+    def get_data(self):
+        return self.agents_python[0].get_data()
+    
+    def get_method(self):
+        raise NotImplementedError()
